6cb83f0c2fdf2953e26f92bdeec8c11caf9c7961
Samuel Ketner <swk6711@utulsa.edu>
2021-09-21 01:40:22 -0500

    Circle resource images + return button event

--- app/src/main/java/com/project/solarsim/MainActivity.java
+++ app/src/main/java/com/project/solarsim/MainActivity.java
@@ -62,9 +62,9 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        //if (id == R.id.action_settings) {
+          //  return true;
+        //}
 
         return super.onOptionsItemSelected(item);
     }
