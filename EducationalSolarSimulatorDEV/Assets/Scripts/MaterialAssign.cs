308c168d14b366389057c3001d1917c07636fef3
Ryan Derr <ryanderr3@gmail.com>
2021-10-19 23:25:55 -0500

    Fix color assigning issue

--- EducationalSolarSimulatorDEV/Assets/Scripts/MaterialAssign.cs
+++ EducationalSolarSimulatorDEV/Assets/Scripts/MaterialAssign.cs
@@ -4,36 +4,13 @@ using UnityEngine;
 
 public class MaterialAssign : MonoBehaviour
 {
-    public Material newMaterial;
+    public Material[] materialList;
     // Start is called before the first frame update
     void Start()
     {
+        int rand = Random.Range(0, 7);
         MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
-        int rand = Random.Range(0, 6);
-        switch (rand)
-        {
-           /* case 0: meshRenderer.material = Black_Moon;
-                break;
-            case 1: meshRenderer.material = Blue_Moon;
-                break;
-            case 2:meshRenderer.material = Blue_Moon; 
-                break;
-            case 3:
-                meshRenderer.material = Blue_Moon ;
-                break;
-            case 4:
-                meshRenderer.material = Blue_Moon;
-                break;
-            case 5:
-                meshRenderer.material = Blue_Moon ;
-                break;
-            case 6:
-                meshRenderer.material = Blue_Moon ;
-                break;
-            default:
-                meshRenderer.material = Blue_Moon;
-                break;*/
-        }
+        meshRenderer.material = materialList[rand];
     }
 
     
