5f57387bf3f969d57c8c428479b3b1e94c719f01
Kacy-Luker <KacyLuker@Gmail.com>
2021-10-30 16:53:19 -0500

    cleaning

--- OrbitLearn/Assets/Scripts/EquationsScene.cs
+++ OrbitLearn/Assets/Scripts/EquationsScene.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class EquationsScrene : MonoBehaviour
+public class EquationsScene : MonoBehaviour
 {
     public void ToEquationScene()
     {
