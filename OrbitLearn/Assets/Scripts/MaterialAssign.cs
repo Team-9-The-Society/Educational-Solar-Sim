1fc8e4c79c9c9d62762f86ca101b1d3deafa8259
Ryan Derr <ryanderr3@gmail.com>
2021-11-15 15:16:30 -0600

    Changed so that trail colors are dependant on the mesh color thus are the same!

--- OrbitLearn/Assets/Scripts/MaterialAssign.cs
+++ OrbitLearn/Assets/Scripts/MaterialAssign.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class MaterialAssign : MonoBehaviour
 {
     public Material[] materialList;
+    public TrailRenderer tail;
     // Start is called before the first frame update
     void Start()
     {
         int rand = Random.Range(0, 7);
         MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
         meshRenderer.material = materialList[rand];
+        Debug.Log(meshRenderer.material.color);
+        tail.startColor = meshRenderer.material.color;
+        tail.endColor = meshRenderer.material.color;
     }
 
     
