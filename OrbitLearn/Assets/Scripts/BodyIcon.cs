501fd8f26647b6946cd291b60bc198bb75612ae4
Kacy-Luker <KacyLuker@Gmail.com>
2022-03-22 14:56:34 -0500

    Added auto scalling so billboards stay the same size

--- OrbitLearn/Assets/Scripts/BodyIcon.cs
+++ OrbitLearn/Assets/Scripts/BodyIcon.cs
@@ -4,16 +4,24 @@ using TMPro;
 public class BodyIcon : MonoBehaviour
 {
     private Transform cam;
+    private float cameraDistance;
+    private RectTransform canvasTransform;
+    private Transform planetIconTransform;
+    private Vector3 tmpLocalScale = new Vector3();
 
     public SphereCollider planetCollider;
     public SpriteRenderer rend;
     public GameObject nameDisplay;
     public float turnOffDistance;
 
+    const float sizeModifier = 75;
+
     private void Start()
     {
         cam = Camera.main.transform;
         GetComponentInParent<Canvas>().worldCamera = Camera.main;
+        canvasTransform = GetComponentInParent<RectTransform>();
+        planetIconTransform = GetComponent<Transform>();
     }
 
     private void LateUpdate()
@@ -23,7 +31,9 @@ public class BodyIcon : MonoBehaviour
         else
             transform.LookAt(transform.position + cam.forward);
 
-        if (Vector3.Distance(cam.transform.position, this.transform.position) < turnOffDistance)
+        cameraDistance = Vector3.Distance(cam.transform.position, planetCollider.transform.position);
+
+        if (cameraDistance < turnOffDistance)
         {
             rend.enabled = false;
             nameDisplay.SetActive(false);
@@ -31,6 +41,14 @@ public class BodyIcon : MonoBehaviour
         }
         else
         {
+            tmpLocalScale.x = cameraDistance / sizeModifier;
+            tmpLocalScale.y = tmpLocalScale.x;
+            tmpLocalScale.z = 1;
+
+            // Apply new scale
+            planetIconTransform.localScale = tmpLocalScale;
+            Debug.Log($"tmpLocalScale {tmpLocalScale.x}\nplanetIconTransform.localScale {canvasTransform.localScale}");
+
             rend.enabled = true;
             nameDisplay.SetActive(true);
             planetCollider.radius = 2.55f;
